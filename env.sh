85ea84ad503c25392ef3ad4a2dd3880ccbb2bad7
Robert Dunmire III <slackr31337@gmail.com>
2023-08-12 21:10:07 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 # Testing on Debian 12 and Python 3.11
 
 sudo apt update
-sudo apt install -y python3 python3-dev python3-venv
+sudo apt install -y python3 python3-dev python3-venv libasound2-dev
 
 
 python3 -m venv .env
@@ -12,4 +12,5 @@ source ./.env/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ./requirements.txt
 
+echo "To run: `python3 ./voice_pipeline.py --server <home-assistant-ip> --pipeline 'HA-Audio-Pipeline`'
 
