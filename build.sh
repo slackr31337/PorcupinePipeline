da082d1bda142dab97d56a6baba7cfbb674da1a2
Robert Dunmire III <slackr31337@gmail.com>
2023-08-13 13:49:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "Warning: Using picovoice in a docker container"
+echo "         will result in consuming a user device"
+echo "         for each container that is run. This"
+echo "         will exhaust a free user's monthly limit"
+
 docker build -f ./docker/Dockerfile -t porcupine-pipeline .
